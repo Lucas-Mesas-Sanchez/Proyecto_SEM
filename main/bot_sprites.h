5629a0f4f2e4b0b335070685ef434b3c2a3bc5f4
Andrés Rico Alastrué <aricala@etsinf.upv.es>
2026-05-08 19:10:23 +0200

    Add bot_sprites.h for robot sprite animations

---
+++ main/bot_sprites.h
@@ -0,0 +1,50 @@
+
+/*INCLUDES RELATIVOS A LOS DISTINTOS SPRITES DEL ROBOT************************/
+
+/*Sprite base*/
+#include "base.h"
+
+/*Includes relativos a la animacion de la antena******************************/
+#include "ant_frame1.h"
+#include "ant_frame2.h"
+#include "ant_frame3.h"
+#include "ant_frame4.h"
+#include "ant_frame5.h"
+#include "ant_frame6.h"
+#include "ant_frame7.h"
+
+
+/*Includes relativos a la animacion de los brazos*****************************/
+
+//brazo izq
+#include "brazo_izq_frame1.h"
+#include "brazo_izq_frame2.h"
+
+//brazo der
+#include "brazo_der_frame1.h"
+#include "brazo_der_frame2.h"
+
+
+/*Includes relativos a las posiciones de los ojos*****************************/
+
+//ojo base 
+#include "ojonormal.h"
+
+//ojo mirando a la izquierda
+#include "ojoizq.h"
+
+//ojo mirando a la derecha
+#include "ojoder.h"
+
+//ojo en cruz
+#include "ojo_cruz.h"
+
+
+/***Bigote********************************************************************/
+#include "boca.h"
+
+/*Versiones de la pantalla*/
+#include "pantalla.h"
+#include "pantalla_pila_alta.h"
+#include "pantalla_pila_baja.h"
+#include "pantalla_pila_vacia.h"
