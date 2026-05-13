059dd96e5cdf29e3f376a474edf94830b499b685
Lucas Mesas Sánchez <lmessan@etsinf.upv.es>
2026-05-13 19:05:28 +0200

    MUCHAS COSAS

--- bot_crtl.c
+++ bot_crtl.c
@@ -16,7 +16,17 @@
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 #define NUM_PHRASES 9
-
+#define PIXEL_TAM   5
+#define OJO_Y       115         
+#define OJO_IX      90
+#define OJO_DX      130
+#define BOCA_X      85
+#define BOCA_Y      145
+#define BRAZO_Y     170
+#define BRAZO_IX    0
+#define BRAZO_DX    160
+#define ANT_X       85
+#define ANT_Y       35
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 
@@ -35,13 +45,54 @@ static const char* phrases_table[] =
 
 };
 static canvas_t bot_canvas;
-/* la estructura de los sprites depende de lo que se quiera hacer con ellos. Si va a ser un sprite estatico que se mueve. se declara normal
-    sprite_t sprite = {vector_sprite(alocado en bot_sprites.h),altura,ancho}
-    si es una animación como por ejemplo la antena recomiendo hacer un vector de sprites. como es mucho copiar y pegar pideselo a la ia que lo haga.
-    el sprite que se dibujará siempre antes que cualquiera será el del robot base, sin extremidades ni ojos. por encima se dibujarán el resto.
-    Para añadir un sprite usa el convertidor del github. lo pones en una carpeta. pones la imagen que quieres convertir y tienes que poner por consola
-    converter.py [foto.extension] <tipo de cuadro (estirado recortado y no se que mas)> <ancho> <alto> o algo así. está en el codigo tmb.
 
+static unsigned int bizq_idx = 0;
+static unsigned int bder_idx = 0;
+static unsigned int antn_idx = 0;
+static sprite_t base           = {base_map, 240, 320};
+
+static sprite_t boca           = {boca_map, 70, 20};
+static sprite_t ojonormal     = {ojonormal_map, 20, 20};
+
+/*
+static const sprite_t ojoder         = {ojoder_map, 20, 20};
+static const sprite_t ojoizq         = {ojoizq_map, 20, 20};
+static const sprite_t ojo_cruz       = {ojo_cruz_map, 20, 20};
+*/
+
+static sprite_t antena_base    = {ant_frame1_map, 70, 40};
+static sprite_t brazo_izq_base = {brazo_izq_frame1_map, 80, 60};
+static sprite_t brazo_der_base = {brazo_der_frame1_map,80, 60};
+
+/*
+static const sprite_t animacion_blink[] = {
+    {ojonormal_map, 20, 20},
+    {blink_frame1_map, 20, 20},
+    {blink_frame2_map, 20, 20},
+    {blink_frame3_map, 20, 20},
+    {blink_frame4_map, 20, 20}
+};
+
+static const sprite_t animacion_antena[] = {
+    {ant_frame1_map, 40, 70},
+    {ant_frame2_map, 40, 70},
+    {ant_frame3_map, 40, 70},
+    {ant_frame4_map, 40, 70},
+    {ant_frame5_map, 40, 70},
+    {ant_frame6_map, 40, 70},
+    {ant_frame7_map, 40, 70},
+};
+
+static const sprite_t animacion_brazo_izq[] = {
+    {brazo_izq_frame1_map, 60, 80},
+    {brazo_izq_frame2_map, 60, 80}
+};
+
+
+static const sprite_t animacion_brazo_der[] = {
+    {brazo_der_frame1_map, 60, 80},
+    {brazo_der_frame2_map, 60, 80}
+};
 */
 /* Private function prototypes -----------------------------------------------*/
 static const sprite_t* next_srpite(const sprite_t* animation,uint8_t* actual,uint8_t size);
@@ -51,10 +102,51 @@ void bot_init(void)
 {
     static uint16_t cdata[] = {0};
     lcd_crtl_canvas_init(&bot_canvas,cdata,240,320);
+    lcd_crtl_canvas_clean(&bot_canvas,0xffff);
+    lcd_crtl_draw_sprite(&bot_canvas, &base, 0, 0);
+    lcd_crtl_draw_sprite(&bot_canvas, &ojonormal, OJO_IX, OJO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &ojonormal, OJO_DX, OJO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &brazo_izq_base, BRAZO_IX, BRAZO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &antena_base, ANT_X, ANT_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &brazo_der_base, BRAZO_DX, BRAZO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &boca, BOCA_X, BOCA_Y);
+    lcd_crtl_canvas_send(&bot_canvas);
+    
 }
 void bot_idle_animation()
 {
+    lcd_crtl_draw_sprite(&bot_canvas, &base, 0, 0);
+   
+    lcd_crtl_draw_sprite(&bot_canvas, &ojonormal, OJO_IX, OJO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &ojonormal, OJO_DX, OJO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &brazo_izq_base, BRAZO_IX, BRAZO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &antena_base, ANT_X, ANT_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &brazo_der_base, BRAZO_DX, BRAZO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &boca, BOCA_X, BOCA_Y);
+    lcd_crtl_canvas_send(&bot_canvas);
+
+    vTaskDelay(pdMS_TO_TICKS(500));
     
+    lcd_crtl_draw_sprite(&bot_canvas, &base, 0, 0);
+    lcd_crtl_draw_sprite(&bot_canvas, &ojonormal, OJO_IX, (OJO_Y - PIXEL_TAM));
+    lcd_crtl_draw_sprite(&bot_canvas, &ojonormal, OJO_DX, (OJO_Y - PIXEL_TAM));
+    lcd_crtl_draw_sprite(&bot_canvas, &brazo_izq_base, BRAZO_IX, BRAZO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &antena_base, ANT_X, ANT_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &brazo_der_base, BRAZO_DX, BRAZO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &boca, BOCA_X, (BOCA_Y - PIXEL_TAM));
+    lcd_crtl_canvas_send(&bot_canvas);
+
+    vTaskDelay(pdMS_TO_TICKS(500));
+
+    lcd_crtl_draw_sprite(&bot_canvas, &base, 0, 0);
+    lcd_crtl_draw_sprite(&bot_canvas, &ojonormal, OJO_IX, OJO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &ojonormal, OJO_DX, OJO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &brazo_izq_base, BRAZO_IX, BRAZO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &antena_base, ANT_X, ANT_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &brazo_der_base, BRAZO_DX, BRAZO_Y);
+    lcd_crtl_draw_sprite(&bot_canvas, &boca, BOCA_X, BOCA_Y);
+    
+    lcd_crtl_canvas_send(&bot_canvas);
 }
 void bot_blink_animation()
 {
---
+++ lcd_crtl.c
@@ -0,0 +1,207 @@
+/**
+    Copyright (C) 2025 The Sistemas Empotrados subject at UPV
+    
+    @file    lecd_crtl.c
+    @author  Lucas Mesas
+    @version V0.1
+    @date    2026-04-25
+    @brief   This file is used to manage a spi ili9341 display more easily
+          
+    This library has been created to manage a spi ili9341 display. It's mainly focused
+    on the drwaing and animating phase more than making it interactive. Also it doesn't
+    manage depth or text labels for now.
+*/
+
+/* Includes ------------------------------------------------------------------*/
+
+#include <stdint.h>
+#include "esp_system.h"
+
+#include "esp_mac.h"
+
+#include "esp_lcd_ili9341.h"
+
+#include "esp_lcd_panel_commands.h"
+#include "esp_lcd_panel_dev.h"
+#include "esp_lcd_panel_interface.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_panel_vendor.h"
+#include "driver/gpio.h"
+#include "esp_vfs_dev.h"
+#include "esp_log.h"
+#include "esp_rom_sys.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "lcd_crtl.h"
+#include "HD44780.h"
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+// MACROS LDR RGB
+#define LCD_HOST SPI2_HOST
+
+#define CS_PIN   GPIO_NUM_7
+#define RST_PIN  GPIO_NUM_15
+#define DC_PIN   GPIO_NUM_8
+#define MOSI_PIN GPIO_NUM_17
+#define SCLK_PIN GPIO_NUM_18
+#define LED_PIN  GPIO_NUM_3   // Movido del 38/1 para evitar conflictos
+#define MISO_PIN GPIO_NUM_16  // (O -1 si no lo usas)
+
+#define LCD_PIXEL_CLOCK_HZ 20*1000*1000 //
+#define LCD_CMD_BITS 8
+#define LCD_PARAM_BITS 8
+
+#define LCD_H_RES 240
+#define LCD_V_RES 320
+
+//MACROS LCD 16x2
+#define LCD_ADDR 0x27
+#define SDA_PIN GPIO_NUM_1
+#define SCL_PIN GPIO_NUM_2
+/* Private variables ---------------------------------------------------------*/
+static esp_lcd_panel_io_handle_t io_handle = NULL;
+static esp_lcd_panel_handle_t panel_handle = NULL;
+static SemaphoreHandle_t dma_sem = NULL;
+/* Private function prototypes -----------------------------------------------*/
+static bool IRAM_ATTR notify_transfer_done(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *smph);
+/* Exported functions --------------------------------------------------------*/
+extern void Cache_WriteBack_Addr(uint32_t addr, uint32_t size);
+void lcd_crtl_display_init(void)
+{
+    /* INICIALIZACION DE PANTALLA LCD 16x2 */
+
+    LCD_init(LCD_ADDR,SDA_PIN,SCL_PIN,16,2);
+    LCD_clearScreen();
+    /* INICIALIZACION DE PANTALLA LCD RBG */
+    dma_sem = xSemaphoreCreateBinary();
+    xSemaphoreGive(dma_sem);
+    
+   
+
+    
+    gpio_set_direction(LED_PIN,GPIO_MODE_OUTPUT);
+        gpio_set_level(LED_PIN,1);
+        spi_bus_config_t buscfg = {
+        .sclk_io_num = SCLK_PIN,
+        .mosi_io_num = MOSI_PIN,
+        .miso_io_num = MISO_PIN,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = LCD_H_RES * LCD_V_RES * sizeof(uint16_t), 
+    };
+        ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
+
+
+    esp_lcd_panel_io_spi_config_t io_config = {
+        .dc_gpio_num = DC_PIN,
+        .cs_gpio_num = CS_PIN,
+        .pclk_hz = LCD_PIXEL_CLOCK_HZ,
+        .lcd_cmd_bits = LCD_CMD_BITS,
+        .lcd_param_bits = LCD_PARAM_BITS,
+        .spi_mode = 0,
+        .trans_queue_depth = 10,
+    };
+    // Attach the LCD to the SPI bus
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
+     const esp_lcd_panel_io_callbacks_t cbs = { // Activamos una funció de callback para poder usar DMA
+        .on_color_trans_done = notify_transfer_done,
+    };
+    ESP_ERROR_CHECK(esp_lcd_panel_io_register_event_callbacks(io_handle, &cbs, dma_sem));
+    esp_lcd_panel_dev_config_t panel_config = {
+        .reset_gpio_num = RST_PIN,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_BGR,
+        .bits_per_pixel = 16,
+    };
+    // Create LCD panel handle for ili9341, with the SPI IO device handle
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ili9341(io_handle, &panel_config, &panel_handle));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    vTaskDelay(pdMS_TO_TICKS(120));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    esp_lcd_panel_set_gap(panel_handle, 0, 0);
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+    vTaskDelay(pdMS_TO_TICKS(50));
+    ESP_LOGI(__FILE__, "Panel iniciado correctamente");
+}
+
+void lcd_crtl_canvas_init(canvas_t* canvas, uint16_t* cdata,uint16_t w,uint16_t h)
+{
+    canvas->width = w;
+    canvas->height = h;
+    size_t raw_size = w * h * sizeof(uint16_t);
+    size_t aligned_size = (raw_size + 63) & ~63;  // ← clave
+
+    canvas->data = (uint16_t *)heap_caps_aligned_alloc(64, aligned_size, MALLOC_CAP_SPIRAM);
+    canvas->aligned_size = aligned_size;
+    if (canvas->data == NULL) {
+        ESP_LOGE("LCD", "¡Error! No se pudo reservar PSRAM");
+    }
+    else
+    {
+        memset(canvas->data, 0, canvas->aligned_size);
+        // Sincronizamos la limpieza inicial
+        Cache_WriteBack_Addr((uint32_t)canvas->data, (uint32_t)canvas->aligned_size);
+    }
+}
+bool lcd_crtl_draw_sprite(canvas_t* canvas, sprite_t* sprite, uint16_t x0, uint16_t y0)
+{
+    bool err = true;
+    //if (((sprite->width + x0) > canvas->width ) || ((sprite->height + y0) > canvas->height))
+   // {
+     //   err = false;
+    //}
+    if (err)
+    {
+        for(uint16_t y = 0; y < sprite->height; y++)
+        {
+            for (uint16_t x = 0; x < sprite->width; x++)
+            {
+                uint32_t canvas_idx = (y0 + y) * canvas->width + (x0 + x);
+                uint32_t sprite_idx = y * sprite->width + x;
+                if (sprite->data[sprite_idx] != LCD_CRTL_TRANSPARENT_COLOR) //Color auxiliar que permite hacer formas no cuadradas. En este caso magenta
+                {
+                    canvas->data[canvas_idx] = sprite->data[sprite_idx];
+                }
+            }
+        }
+    }
+    return err;
+}
+
+void lcd_crtl_canvas_clean(canvas_t* canvas, uint16_t color)
+{
+    for(uint32_t y = 0; y < canvas->height; y++)
+        {
+            for (uint32_t x = 0; x < canvas->width; x++)
+            {
+                uint32_t canvas_idx = y * canvas->width + x;
+                canvas->data[canvas_idx] = color;
+            }
+        }
+}
+
+void lcd_crtl_canvas_send(canvas_t* canvas)
+{
+    Cache_WriteBack_Addr((uint32_t)canvas->data, canvas->aligned_size);
+    xSemaphoreTake(dma_sem, portMAX_DELAY);
+    esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, canvas->width, canvas->height, canvas->data);
+}
+
+
+/* Private functions ---------------------------------------------------------*/
+static bool IRAM_ATTR notify_transfer_done(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *smph)
+{
+    BaseType_t high_task_wakeup = pdFALSE;
+    
+    xSemaphoreGiveFromISR((SemaphoreHandle_t)smph, &high_task_wakeup);
+    return high_task_wakeup == pdTRUE;
+}
+
+
+
+/* End of file ****************************************************************/
+
