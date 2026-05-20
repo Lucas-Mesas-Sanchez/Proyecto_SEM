2c4f89cf8985787f8148503e4019f8ee68870697
Kyrylo Korolov <kkorolo@etsinf.upv.es>
2026-05-20 18:18:09 +0200

    Refactor WiFi initialization and event handling. WIFI AHORA FUNCIONA

--- main/Wifi.c
+++ main/Wifi.c
@@ -8,13 +8,10 @@
 */
 
 /* NOTE: This file is very nyapeada por Angel Perles. It needs some extra love */
-//este codigo esta copiado del codigo de Angel Perles
-
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-#include "esp_mac.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
@@ -24,8 +21,8 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-#include "Wifi.h"
-#include "Wifi_credentials.h"
+#include "wifi.h"
+#include "wifi_credentials.h"
 
 /* The examples use WiFi configuration that you can set via project configuration menu
 
@@ -89,12 +86,13 @@ static void wifi_init_sta(void);
 
 void wifi_init(void)
 {
-    bool first_time = true;
+    static bool first_time = true;
 
     if (!first_time)
     {
         return;
     }
+    first_time = false;
 
     //Initialize NVS
     esp_err_t ret = nvs_flash_init();
@@ -168,19 +166,16 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        wifi_event_sta_disconnected_t *disconn = (wifi_event_sta_disconnected_t *)event_data;
         connected = false;
-        //if (s_retry_num < ESP_MAXIMUM_RETRY) {
-        if (true) {
-            vTaskDelay(CONNECT_RETRY_TIME/portTICK_PERIOD_MS);
+        if (s_retry_num < ESP_MAXIMUM_RETRY) {
             esp_wifi_connect();
             s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-            // NOTE: afegit per mi
-            // connected = false;  
+            ESP_LOGW(TAG, "[reason=%d] retry %d/%d", disconn->reason, s_retry_num, ESP_MAXIMUM_RETRY);
         } else {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            ESP_LOGE(TAG, "[reason=%d] max retries reached", disconn->reason);
         }
-        ESP_LOGI(TAG,"connect to the AP fail");
 
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
@@ -225,7 +220,7 @@ static void wifi_init_sta(void)
              * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
              * WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards.
              */
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK,   // ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,  
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,   // iPhone hotspot uses WPA2
             .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,           // ESP_WIFI_SAE_MODE,
             .sae_h2e_identifier = "",                   // EXAMPLE_H2E_IDENTIFIER,
         },
@@ -236,6 +231,25 @@ static void wifi_init_sta(void)
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
 #if 0
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 WIFI_SSID, WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 WIFI_SSID, WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
 #endif    
 }
 
